154d152b3d9c05b3916a048e66d85f88dc73162c
victor santiago <victorhenriquedejesussantiago@gmail.com>
2026-07-19 03:04:49 -0300

    feat: Add AggregationResultJson column to PipelineRun for enhanced data tracking

---
+++ src/IntegrationConnector.Infrastructure/Migrations/20260713234409_AddAggregationResult.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntegrationConnector.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAggregationResult : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AggregationResultJson",
+                table: "pipeline_runs",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AggregationResultJson",
+                table: "pipeline_runs");
+        }
+    }
+}
